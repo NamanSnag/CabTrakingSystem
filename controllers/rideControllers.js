65d5c60bb68fb038b437b088049ca2d9f42e9b8c
Naman Nag <namannag@Namans-MacBook-Air.local>
2025-03-15 06:08:23 +0530

    feat: minor change

--- controllers/rideControllers.js
+++ controllers/rideControllers.js
@@ -1,31 +1,30 @@
 import prisma from "../configs/dbConfig.js";
+import { successResponse } from "../utils/responses.js";
 
+export const getActiveCabs = async (req, res, next) => {
+  try {
+    const activeCabs = await prisma.driver.findMany({
+      where: {
+        active: true,
+        isBooked: false,
+      },
+      include: {
+        car: true, // Include the associated car details
+      },
+    });
 
+    // Map the response to include driver + car details
+    const response = activeCabs.map((driver) => ({
+      id: driver.id,
+      name: driver.name,
+      isBooked: driver.isBooked,
+      location: { lat: driver.lat, lng: driver.lng },
+      car: driver.car, // Include car details like numberPlate, model, etc.
+    }));
 
-export const getActiveCabs = async (req, res) => {
-    try {
-      const activeCabs = await prisma.driver.findMany({
-        where: { 
-          active: true,
-          isBooked: false 
-        },
-        include: {
-          car: true // Include the associated car details
-        }
-      });
-  
-      // Map the response to include driver + car details
-      const response = activeCabs.map(driver => ({
-        id: driver.id,
-        name: driver.name,
-        isBooked: driver.isBooked,
-        location: { lat: driver.lat, lng: driver.lng },
-        car: driver.car // Include car details like numberPlate, model, etc.
-      }));
-  
-      res.json(response);
-    } catch (error) {
-      console.error("Error fetching active cabs:", error);
-      res.status(500).json({ error: "Internal server error" });
-    }
-  };
+    return res.status(200).json(successResponse(response, "list of active cabs"));
+  } catch (error) {
+    console.error("Error fetching active cabs:", error);
+    next(error);
+  }
+};
